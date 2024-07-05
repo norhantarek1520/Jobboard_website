876a8bc8a3c669fbe6d7848d8372e5acf601de7b
nourhan tarek <norhantarek1520@gmail.com>
2024-07-05 10:55:18 +0300

    adding morgan middleware that acts as an HTTP request logger

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const cors = require("cors");
-const bodyParser = require('body-parser');
+const cors = require("cors");//Enables controlled access to resources on a server from a web page running on a different domain.
+const bodyParser = require('body-parser');//Parses incoming request bodies
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv') //facilitates managing environment variables from a .env file.
 const app = express();
-
+const morgan = require('morgan'); // Morgan is a popular middleware that acts as an HTTP request logger
 // Global middleware
 app.use(cors());
 dotenv.config()
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("uploads")); // Serve static files from uploads
-
+app.use(morgan('dev'));
 // Test HTTP Req 
 app.post('/test',(req,res)=>{
     console.log("This is Jobboard app , who are you ? ")
